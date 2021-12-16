b62e15e97d0fdb64f1cbd27c97b9fd3975d1a5f8
LanceFyre99 <lance.fyre@live.com>
2021-12-15 17:00:02 -0800

    revised vibe charting, added fate history

--- application.py
+++ application.py
@@ -26,9 +26,11 @@ class player_chronicle():
     #A Blaseball player, and all the relevant stats except vibes (since those change too frequently to be encapsulated by a static value)
     #Created using dicts returned by Chronicler.
     def __init__(self, dict):
-        self.fateChange = "Pre-change."
+        self.fateChange = None
         self.name = dict["data"]["name"]
-        self.time = dict["lastSeen"]
+        self.timestamp = dict["lastSeen"]
+        self.date = self.timestamp[0:10]
+        self.time = self.timestamp[11:19]
         if "leagueTeamId" in dict["data"]:
             self.teamID = dict["data"]["leagueTeamId"]
         else:
@@ -40,18 +42,18 @@ class player_chronicle():
         self.id = dict["playerId"]
         if "fate" in dict["data"]:
             if dict["data"]["fate"]:
-                self.fate = float(dict["data"]["fate"])
+                self.fate = int(dict["data"]["fate"])
             else:
-                self.fate = "NONE"
+                self.fate = "none, according to the API. In reality, they likely had one that just wasn't tracked. Chronicler is inconsistent at times"
         else:
-            self.fate = "NOT YET TRACKED"
+            self.fate = "not yet tracked by Chronicler"
         self.modifications = []
         for key in ["gameAttr","permAttr","seasAttr","weekAttr"]:
             if key in dict["data"]:
                 for mod in dict["data"][key]:
                     self.modifications.append(mod)
     def __str__(self):
-        return f"""{self.name}   ({self.time})
+        return f"""{self.name}   ({self.date} at {self.time})
         ID: {self.id}
         Team ID: {self.teamID}
         Pregame ritual: {self.ritual}
@@ -60,15 +62,23 @@ class player_chronicle():
         Reason for change to Fate: {self.fateChange}\n"""
 
 class team_record():
-    #A Blaseball team. Stores name & ID.
-    def __init__(self, dict):
+    #Stores name and ID for a blaseball team or location, as well as if it's a team or location
+    def __init__(self, dict, type = 'team'):
         self.name = dict["full_name"]
         self.id = dict["team_id"]
+        self.status = dict['team_current_status']
+        self.emoji = dict['team_emoji']
+        self.type = type
     def __str__(self):
         #Returns team name as string
         return self.name
     def __repr__(self):
         return self.name
+
+def area_record(name, emoji, id):
+    #defines a team_record class object for a location (such as the hall of flame)
+    area = {"full_name": name, "team_id": id, "team_current_status": None, "team_emoji": emoji}
+    return team_record(area, type='area')
 
 def get_player_history(id, page = None):
     try:
@@ -92,8 +102,8 @@ def get_player_history(id, page = None):
 
 def roster_swap_doublecheck(player):
     try:
-        paramstr1 = urllib.parse.urlencode({'player': player.id, 'count': 1, 'before': player.time})
-        paramstr2 = urllib.parse.urlencode({'player': player.id, 'count': 1, 'after': player.time})
+        paramstr1 = urllib.parse.urlencode({'player': player.id, 'count': 1, 'before': player.timestamp})
+        paramstr2 = urllib.parse.urlencode({'player': player.id, 'count': 1, 'after': player.timestamp})
         baseurl = 'https://api.sibr.dev/chronicler/v1/roster/updates'
         request1 = baseurl+'?'+paramstr1
         requeststr1 = urllib.request.urlopen(request1).read()
@@ -136,28 +146,53 @@ def get_full_player_history(id):
     cleanput = [player_chronicle(entry) for entry in flatput]
     return cleanput
 
-def fate_filtered_history(list):
+def fate_filtered_history(input):
     output = []
     last_entry = None
-    for entry in list:
-        if entry.fate != "NOT YET TRACKED":
+    for entry in input:
+        if entry.fate not in ['not yet tracked by Chronicler','none, according to the API. In reality, they likely had one that just wasn\'t tracked. Chronicler is inconsistent at times']:
             if last_entry != None:
-                if last_entry.fate != entry.fate:
-                    if output == [] or output[-1].time != last_entry.time:
+                if last_entry.fate != entry.fate and last_entry.fate != "NONE":
+                    if output[-1].timestamp != last_entry.timestamp:
                         output.append(last_entry)
                     if 'ALTERNATE' in entry.modifications and 'ALTERNATE' not in last_entry.modifications:
-                        entry.fateChange = "Stat randomization due to becoming an Alternate."
+                        entry.fateChange = "due to becoming an Alternate."
                     elif entry.teamID != last_entry.teamID:
-                        entry.fateChange = "Fate reroll due to Feedback swap."
+                        entry.fateChange = "due to a Feedback swap."
                     else:
                         feedback_doublecheck = roster_swap_doublecheck(entry)
                         if feedback_doublecheck:
-                            entry.fateChange = "Fate reroll due to Feedback swap."
+                            entry.fateChange = "due to a Feedback swap."
                         else:
-                            entry.fateChange = "Outlier. Cause cannot be clearly determined."
+                            if entry.name in ['Axel Trololol','Lachlan Shelton','Antonio Wallace','Hobbs Cain']:
+                                entry.fateChange = "due to a Feedback swap."
+                            else:
+                                entry.fateChange = "due to... some other cause. (If you're seeing this, odds are that there's been an error.)"
                     output.append(entry)
+            else:
+                output.append(entry)
             last_entry = entry
+    #people's names change and I'd like to make sure I use the most recent name.
+    output[0].name = input[-1].name
     return output
+
+def get_pooled_players(pool = 'deceased'):
+    #This defaults to getting all dead players.
+    try:
+        paramstr = paramstr = urllib.parse.urlencode({'playerPool': pool})
+        baseurl = 'https://api.blaseball-reference.com/v2/players'
+        request = baseurl+'?'+paramstr
+        requeststr = urllib.request.urlopen(request).read()
+        data = json.loads(requeststr)
+    except urllib.error.URLError as e:
+        if hasattr(e,"code"):
+            print("The server couldn't fulfill the request.")
+            print("Error code: ", e.code)
+        elif hasattr(e,'reason'):
+            print("We failed to reach a server")
+            print("Reason: ", e.reason)
+    else:
+        return data
 
 def get_player(id = None):
     #This gets EVERY player if it doesn't get an ID.
@@ -217,13 +252,29 @@ def clean_team_list(season = 23):
     clean_list = [team_record(team) for team in team_list]
     return clean_list
 
+def season_mapping():
+    try:
+        request = 'https://api.sibr.dev/chronicler/v1/time/seasons'
+        requeststr = urllib.request.urlopen(request).read()
+        data = json.loads(requeststr)
+    except urllib.error.URLError as e:
+        if hasattr(e,"code"):
+            print("The server couldn't fulfill the request.")
+            print("Error code: ", e.code)
+        elif hasattr(e,'reason'):
+            print("We failed to reach a server")
+            print("Reason: ", e.reason)
+    else:
+        del data['data'][-2:]
+        return data['data']
+
+"""
 def summarize_player(player):
     if float(player['batting_stars']) >= 5.0:
         print(pretty(player))
         return f'----------------------------\nName: {player["player_name"]}\nTeam: {player["team"]}\nPregame Ritual: {player["ritual"]}\nBatting Stars: {player["batting_stars"]}\n'
 
 
-"""
 with open("batters.txt", "w") as text_file:
     idlist = clean_team_list()
     for team in idlist:
@@ -235,26 +286,44 @@ with open("batters.txt", "w") as text_file:
                 text_file.write(summary)
 """
 @app.route("/")
-def league_list():
+def landing():
     app.logger.info("On Main Page!")
-    title = 'Team IDs'
-    #pulls team list for season 13, manually removes the Hall Stars and THE PODS as they're not active teams.
-    #season 13 is used rather than the latest season to avoid unnecessarily pulling pre-historical teams.
-    data = clean_team_list(13)
-    for team in data:
-        if team.name == "THE SHELLED ONE'S PODS" or team.name == "The Hall Stars":
-            app.logger.info(f"***Removing {team} from roster")
-            data.remove(team)
-    return render_template('base_page.html',title=title,data=data)
+    title = 'HCDE 310 Final: Blaseball Vibes & Fate'
+    return render_template('origin.html',title=title)
 
-@app.route("/groster")
-def roster_printer():
+@app.route("/vteam")
+def vleague_list():
+    title = 'Vibe Analysis Tool'
+    #desc goes here
+    data = clean_team_list()
+    outdata = []
+    for team in data:
+        if team.name == "The Hall Stars":
+            team.name = "Hall Stars"
+            app.logger.info(f"***Editing the Hall Stars!")
+        if team.name != "Hall Stars" and team.status != 'active':
+            app.logger.info(f"***Removing {team} from roster")
+        else:
+            app.logger.info(f"***{team} remains. They are {team.status}")
+            outdata.append(team)
+    outdata.append(area_record("Vault", "0x1F947", '698cfc6d-e95e-4391-b754-b87337e4d2a9'))
+    outdata.append(area_record("Hall of Flame", "0x1f480", id = 'Hell'))
+    return render_template('vteam_select.html',title=title,data=outdata)
+
+@app.route("/vroster")
+def vroster_printer():
     team = request.args.get("selected_team")
     print(team)
-    app.logger.info(f"Getting roster for {team}")
-    raw_roster = get_team_roster(team, includeShadows=True)
+    app.logger.info(f"Getting roster for the {team}")
+    if team == "Hell":
+        raw_roster = get_pooled_players()
+        for player in raw_roster:
+            if player['team'] != 'null':
+                raw_roster.remove(player)
+    else:
+        raw_roster = get_team_roster(team, includeShadows=True)
     clean_roster = [player_record(player) for player in raw_roster]
-    return render_template('roster_return.html',title=f"Roster for the {team}",roster=clean_roster,team=team)
+    return render_template('vroster_return.html',title=f"Roster for the {team}",roster=clean_roster,team=team)
 
 @app.route("/gvibes")
 def vibe_charts():
@@ -283,36 +352,77 @@ def vibe_charts():
     chart_base64 = base64.b64encode(chart_bytes.read())
     chart_bytes.close()
     return render_template('vibechart_template.html',title=f"Summary of {player.name}",player=player, graph=chart_base64.decode('utf8'))
-"""
-MalikID = '1301ee81-406e-43d9-b2bb-55ca6e0f7765'
-hist = get_full_player_history(MalikID)
-fate_hist = fate_filtered_history(hist)
-for entry in fate_hist:
-    print(entry)
-"""
 
-league_fate = []
-idlist = clean_team_list(13)
-print("Got team list!")
-for team in idlist:
-    print(team.name)
-    roster = get_team_roster(team.id)
-    for player in roster:
-        print(player["player_name"])
-        hist = get_full_player_history(player["player_id"])
-        fate_hist = fate_filtered_history(hist)
-        if len(fate_hist) > 1:
-            for entry in fate_hist:
-                league_fate.append(entry)
+@app.route("/fteam")
+def fleague_list():
+    title = 'Fate Summary Tool'
+    #desc goes here
+    data = clean_team_list()
+    outdata = []
+    for team in data:
+        if team.name == "The Hall Stars":
+            team.name = "Hall Stars"
+            app.logger.info(f"***Editing the Hall Stars!")
+        if team.name != "Hall Stars" and team.status != 'active':
+            app.logger.info(f"***Removing {team} from roster")
+        else:
+            app.logger.info(f"***{team} remains. They are {team.status}")
+            outdata.append(team)
+    outdata.append(area_record("Vault", "0x1F947", '698cfc6d-e95e-4391-b754-b87337e4d2a9'))
+    outdata.append(area_record("Hall of Flame", "0x1f480", id = 'Hell'))
+    return render_template('fteam_select.html',title=title,data=outdata)
 
-with open("fate.txt", "w") as text_file:
-    for entry in league_fate:
-        text_file.write(str(entry))
+@app.route("/froster")
+def froster_printer():
+    team = request.args.get("selected_team")
+    print(team)
+    app.logger.info(f"Getting roster for the {team}")
+    if team == "Hell":
+        raw_roster = get_pooled_players()
+        for player in raw_roster:
+            if player['team'] != 'null':
+                raw_roster.remove(player)
+    else:
+        raw_roster = get_team_roster(team, includeShadows=True)
+    clean_roster = [player_record(player) for player in raw_roster]
+    return render_template('froster_return.html',title=f"Roster for the {team}",roster=clean_roster,team=team)
 
-"""
+@app.route("/fhist")
+def fate_summary():
+    player_id = request.args.get("selected_player")
+    hist = get_full_player_history(player_id)
+    fate_hist = fate_filtered_history(hist)
+    return render_template('fate_history.html',title=f"Vibe summary for {fate_hist[0].name}",history=fate_hist, length = len(fate_hist))
+#@app.route("/fseason")
+
+#@app.route("/fscatter")
+
+#@app.route("/review")
+
+#MalikID = '1301ee81-406e-43d9-b2bb-55ca6e0f7765'
+#hist = get_full_player_history(MalikID)
+#fate_hist = fate_filtered_history(hist)
+#for entry in fate_hist:
+#    print(entry)
+
+
+#league_fate = []
+#idlist = get_player()
+#for player in idlist:
+#    print(player["player_name"])
+#    hist = get_full_player_history(player["player_id"])
+#    fate_hist = fate_filtered_history(hist)
+#    if len(fate_hist) > 1:
+#        for entry in fate_hist:
+#            league_fate.append(entry)
+
+#with open("fate.txt", "w") as text_file:
+#    for entry in league_fate:
+#        text_file.write(str(entry))
+
+
 if __name__ == "__main__":
     # Used when running locally only.
     # When deploying to Google AppEngine, a webserver process will
     # serve your app.
     app.run(host="localhost", port=8080, debug=True)
-"""
